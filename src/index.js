43f0de2bde9d32219c586f698e970f5cdb59cb37
Sumit-9900 <paulsumit9900@gmail.com>
2025-06-11 21:32:01 +0530

    add cookie-parser and cors dependencies; setup app middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import 'dotenv/config';
 import connectDB from "./db/db.js";
+import { app } from './app.js';
+
+const port = process.env.PORT || 8000
 
 connectDB()
+.then(() => {
+    app.on('error', (error) => {
+        console.log("ERRR: ", error);
+        process.exit(1)
+    })
+
+    app.listen(port, () => {
+        console.log(`The app is listening at port: ${port}`)
+    })
+})
+.catch((error) => {
+    console.log(`MONGODB CONNECTION FAILED: ${error}`)
+})
